b8c6b4a392f7e7b6e90796cf8a71a30e3234e150
Lenovo <altynbekuulubajsal@gmail.com>
2022-10-05 14:18:40 +0300

    123

--- src/entities/DeliveryCompany.java
+++ src/entities/DeliveryCompany.java
@@ -3,15 +3,14 @@ package entities;
 import java.util.List;
 
 public class DeliveryCompany {
-    public final static double PRICE_PER_KILOGRAM = 20;
-    public final static double MAX_CAPACITY_PER_SQUARE_SANTIMETER = 35;
+    public final static double PRICE_PER_KILOGRAM = 20.0;
+    public final static double MAX_CAPACITY_PER_SQUARE_SANTIMETER = 35.0;
     private String deliveryCompanyName;
     private List<Customer> customers;
 
     public DeliveryCompany(String deliveryCompanyName, List<Customer> customers) {
         this.deliveryCompanyName = deliveryCompanyName;
         this.customers = customers;
-        System.out.println("HELLO WORLD!");
     }
 
     public String getDeliveryCompanyName() {
