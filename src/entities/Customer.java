6309dfe4c4d7f5a5e9ff13a5dbaf2481d24bdd92
Mamatova Nuraiym <mnurajym9@gmail.com>
2022-10-05 16:50:37 +0600

    Initial commit

---
+++ src/entities/Customer.java
@@ -0,0 +1,29 @@
+package entities;
+
+import java.util.List;
+
+public class Customer {
+    private String customerName;
+    private List<Order> orders;
+
+    public Customer(String customerName, List<Order> orders) {
+        this.customerName = customerName;
+        this.orders = orders;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void addOrders(Order order) {
+        orders.add(order);
+    }
+}
