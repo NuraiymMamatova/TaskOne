fe6779554c4c1d9e597a4930815453010b4d672c
Lenovo <altynbekuulubajsal@gmail.com>
2022-10-06 07:56:32 +0300

    orderfee-changed

--- src/service/DeliveryCompanyService.java
+++ src/service/DeliveryCompanyService.java
@@ -10,5 +10,6 @@ public class DeliveryCompanyService {
 
     public void acceptOrder(Order order,int company,int custom){
         if(order.getLoad().getHeight()*order.getLoad().getWidth()<DeliveryCompany.MAX_CAPACITY_PER_SQUARE_SANTIMETER) deliveryCompanies.get(company).getCustomers().get(custom).addOrders(order);
+
     }
 }
